11f893d44a39391c201192c33e13484971073c0c
Nantha96 <nanthakmr2@gmail.com>
2020-11-30 13:25:20 +0530

    Changed module key_pair to deployer one

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ module "key_pair" {
 
   source = "terraform-aws-modules/key-pair/aws"
 
-  key_name   = "deployer-two"
+  key_name   = "deployer-one"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAiAistfEpeFZJQy9LvplAwVwPcALRbp7sN6E5Nw1Rf1/PQc8TFsfONo+FNXeqIuGbgZNpAglsNROyHQaldMDHlDF/GtUzsBpCplSYweDPcmhlLt9NToGqyZ+YA9VYzWdC20Sl/bajs9L3nuwJIaO0Gw7rbhbSMKwuGCJrNjJrvazj5yR5hvopJfdsWriCVekhdr/GqsKh651RE/vHRFzmPvlKUwciIHsYrt4sWv3Tl9PWnHT8uwx4xDE8KxEhD1ZaV66C8YPSpHearkSbpMLhXmMrU3GSS1Po108KBw2JYvNn7mIHNQ511Ag9bIkA/1TF9yHFlG8fVhjGWC8x5B/yvw=="
 
 }
