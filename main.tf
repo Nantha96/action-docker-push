23ae615a886d54bc349b91b07254cf446824fc25
Nantha96 <nanthakmr2@gmail.com>
2020-11-30 13:22:08 +0530

    deployer two to deployer one

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "myInstance" {
   ami           = "ami-0a91cd140a1fc148a"
   instance_type = "t2.micro"
-  key_name = "deployer-two"
+  key_name = "deployer-one"
 }		
 
 provider "aws" {
