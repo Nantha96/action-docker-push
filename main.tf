a02c4ad4029c42d3650265bf26e2d37c98534241
Nantha96 <nanthakmr2@gmail.com>
2021-03-26 16:25:15 +0530

    Updated new bucket

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "nanthabucket"
+    bucket         = "nanthabucketone"
     key            = "terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "terraform-db"
